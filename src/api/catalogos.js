19069675983721677da0ac1da1ec60fbd0eb5beb
jnarvaez <benxiin215@gmail.com>
2025-08-25 00:11:13 -0500

    add consumos de catalogos de subcategoria y tipos de movimiento

--- src/api/catalogos.js
+++ src/api/catalogos.js
@@ -7,6 +7,34 @@ export const getAccountsSelect = async () => {
             Authorization: `Bearer ${token}`,
             },
         });
-    console.log(res)
     return res.json();
 }
+
+export const getCategoriesSelect = async () => {
+    const res = await fetch(`${API_URL}/categorySelect`, {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            },
+        });
+    return res.json();
+}
+
+export const getSubCategoriesSelect = async () => {
+    const res = await fetch(`${API_URL}/subcategorySelect`, {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            },
+        });
+    return res.json();
+}
+
+export const getTypesMovement = [
+    {
+      idType: 'I',
+      description: "Ingreso"
+    },
+    {
+      idType: 'G',
+      description: "Gasto"
+    }
+];
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,5 +1,18 @@
-const Select = ({ options, value, onChange, labelKey = "label", valueKey = "value", placeholder = "Seleccione..." }) => (
-    <select value={value} onChange={(e) => onChange(e.target.value)} className="form-select">
+const Select = ({
+    options,
+    value,
+    onChange,
+    labelKey = "label",
+    valueKey = "value",
+    placeholder = "Seleccione...",
+    disabled=false
+}) => (
+    <select 
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        className="form-select"
+        disabled={disabled}
+    >
         <option value="">{placeholder}</option>
         {options.map((opt) => (
             <option key={opt[valueKey]} value={opt[valueKey]}>
