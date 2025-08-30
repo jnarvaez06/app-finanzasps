e45116ee12f2c3fabd0a78fc4ce2d8fbc7f35cc7
jnarvaez <juliannarvaezmazo@hotmail.com>
2025-08-30 00:06:42 -0500

    Ajustes NumberInput y envio de Movimientos al backend

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,22 +1,21 @@
 export default function NumberInput(props) {
     const formatNumber = (val) => {
-      if (!val) return "";
-      return new Intl.NumberFormat("es-CO").format(val);
+        if (!val) return "";
+        return new Intl.NumberFormat("es-CO").format(val);
     };
-  
+
     const handleInput = (e) => {
-      // mantén solo números
-      const rawValue = e.target.value.replace(/\D/g, "");
-      // asigna al input formateado
-      e.target.value = formatNumber(rawValue);
+        // mantén solo números
+        const rawValue = e.target.value.replace(/\D/g, "").replace(",", ".");
+        // asigna al input formateado
+        e.target.value = formatNumber(rawValue);
     };
-  
+
     return (
-      <input
+    <input
         type="text"
         onInput={handleInput}
         {...props}
-      />
+    />
     );
-  }
-  
+}
--- src/hooks/useMovimientos.jsx
+++ src/hooks/useMovimientos.jsx
@@ -1,28 +1,46 @@
-// hooks/useMovimientos.js
 import { useState } from 'react';
 import { API_URL } from '../api/config';
 
 export function useMovimientos() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-  const guardarMovimiento = async (data) => {
-    try {
-      setLoading(true);
-      const res = await fetch(`${API_URL}/movement`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      });
-      if (!res.ok) throw new Error('Error al guardar el movimiento');
-      return await res.json();
-    } catch (err) {
-      setError(err.message);
-      throw err;
-    } finally {
-      setLoading(false);
-    }
-  };
+	const guardarMovimiento = async (data) => {
+		try {
+			setLoading(true);
+			
+			const token = sessionStorage.getItem('token');
+			const username = sessionStorage.getItem('username');
+			const value = parseFloat(data.monto.replace(/\./g, "").replace(",", "."));
 
-  return { guardarMovimiento, loading, error };
+			const postData = {
+				idAccount:data.accountId,
+				description:data.descripcion,
+				type:data.typeMovementId,
+				dateMovement:data.fecha,
+				value:value,
+				idCategory:data.categoryId,
+				idSubCategory:data.subCategoryId,
+				user:username
+			};
+
+			const res = await fetch(`${API_URL}/movement`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify(postData),
+			});
+			if (!res.ok) throw new Error('Error al guardar el movimiento');
+			return await res.json();
+		} catch (err) {
+			setError(err.message);
+			throw err;
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return { guardarMovimiento, loading, error };
 }
